1676187c0a23513a6ccdd2b57e5e2890470d9864
5xians <786828962@qq.com>
2021-07-10 11:52:42 +0800

    .

--- jd_summer_movement_help.js
+++ jd_summer_movement_help.js
@@ -54,7 +54,7 @@ const UA = $.isNode() ? (process.env.JD_USER_AGENT ? process.env.JD_USER_AGENT :
       '本脚本只助力SH\n' +
       '百元守卫战 开启时间早上8点过后\n' +
       '活动时间：2021-07-08至2021-08-08\n' +
-      '脚本更新时间：2021年7月10日 02点00分\n'
+      '脚本更新时间：2021年7月10日 12点00分\n'
       );
       if(Number(summer_movement_ShHelpFlag) === 1){
         console.log('您设置了 【百元守卫战SH】✅ || 互助✅')
